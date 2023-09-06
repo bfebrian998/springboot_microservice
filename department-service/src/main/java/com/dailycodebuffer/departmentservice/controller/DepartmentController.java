c6bfe00423c814b2bca4b6f6ca0c613717fd4a70
unknown <bfebrian998@gmail.com>
2023-09-06 10:49:28 +0700

    microservice

---
+++ department-service/src/main/java/com/dailycodebuffer/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,37 @@
+package com.dailycodebuffer.departmentservice.controller;
+
+import com.dailycodebuffer.departmentservice.model.Department;
+import com.dailycodebuffer.departmentservice.repository.DepartmentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+    private  static final Logger LOGGER
+            = LoggerFactory.getLogger(DepartmentController.class);
+    @Autowired
+    private DepartmentRepository repository;
+
+    @PostMapping
+    public Department add(@RequestBody Department department){
+        LOGGER.info("Depar,emt add: {}", department);
+        return  repository.addDepartment(department);
+    }
+
+    @GetMapping
+    public List<Department> findAll(){
+        LOGGER.info("Department find");
+        return  repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public  Department findById(Long id){
+        LOGGER.info("Department find: id={}",id);
+        return  repository.findById(id);
+    }
+}
